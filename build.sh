5fae8c83e8efce50e9830910de9dce35c59a6ef7
carlos <charlygomez776@gmail.com>
2025-11-21 11:08:45 -0600

    nuevo

--- build.sh
+++ build.sh
@@ -1,12 +1,8 @@
 #!/usr/bin/env bash
-# Exit on error
 set -o errexit
 
-# Instalar dependencias
 pip install -r requirements.txt
 
-# Aplicar migraciones
-python manage.py migrate --no-input
-
-# Recolectar archivos estáticos
 python manage.py collectstatic --no-input
+
+python manage.py migrate
